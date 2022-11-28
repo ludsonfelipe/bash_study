5bb1a225a1e752dc851d26c386e2636353b6f9b0
ludsonfelipe <felipe_ludson@hotmail.com>
2022-11-28 18:28:59 -0300

    new

--- array.bash
+++ array.bash
@@ -1,7 +1,36 @@
-my_array=(1 2 3)
+#!/usr/bash
+
+# Creating an array
+# -a represent a array
+declare -a my_array=(1 2 3)
+
+# Show all values in array
+echo ${my_array[@]}
+
+# Show number in one index
+echo ${my_array[0]}
+
+# Len array
+echo ${#my_array[@]}
+
+# Alter numbers in a index
+my_array[0]=100
 
 echo ${my_array[@]}
 
-echo ${my_array[0]}
+# Slicing array :start_index:num_index
+echo ${my_array[@]:1:2}
 
-echo ${#my_array[@]}
+# Appending in array
+my_array+=(10)
+echo ${my_array[@]}
+
+# Associative array in bash (like a dictonarys)
+# -A represents a Associative array
+declare -A my_dict_array=([column_name]="Name Column" [column_value]=10)
+
+# Keys
+echo ${!my_dict_array[@]}
+
+# Values
+echo ${my_dict_array[@]}
